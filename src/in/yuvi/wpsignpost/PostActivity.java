632d04c456d03d6e3aacd489689dba4cd725c083
YuviPanda <yuvipanda@gmail.com>
2012-08-04 19:38:36 +0530

    Added Share action to pages

--- src/in/yuvi/wpsignpost/PostActivity.java
+++ src/in/yuvi/wpsignpost/PostActivity.java
@@ -8,10 +8,12 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.*;
+import android.widget.ShareActionProvider;
 import android.support.v4.app.NavUtils;
 
 public class PostActivity extends Activity {
 	private WebView webview;
+	private Post post;
 	
 	private class PostWebViewClient extends WebViewClient {
 		@Override
@@ -23,6 +25,20 @@ public class PostActivity extends Activity {
 	}
 
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+	     getMenuInflater().inflate(R.menu.activity_post, menu);
+	        ShareActionProvider prov = (ShareActionProvider) menu.findItem(R.id.menu_share).getActionProvider();
+	        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+	        shareIntent.setAction(Intent.ACTION_SEND);
+	        shareIntent.setType("text/plain");
+	        shareIntent.putExtra(Intent.EXTRA_TEXT, post.permalink);
+	        shareIntent.putExtra(Intent.EXTRA_TITLE, post.title);
+	        
+	        prov.setShareIntent(shareIntent);
+
+	        return true;
+	    }
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         webview = new WebView(this);
@@ -31,8 +47,8 @@ public class PostActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         
         Intent intent = getIntent();
-        Post p = (Post)intent.getExtras().get("post");
-        webview.loadDataWithBaseURL("http://en.wikipedia.org", p.content, "text/html", "utf-8", null);
+        post = (Post)intent.getExtras().get("post");
+        webview.loadDataWithBaseURL("http://en.wikipedia.org", post.content, "text/html", "utf-8", null);
     }
     
     @Override
